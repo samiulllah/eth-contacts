4bdfdc9d5a2efdaa3ac79f12193887b9536894f7
samiulllah <61376705+samiulllah@users.noreply.github.com>
2022-02-15 12:00:22 +0500

    testnet-version

--- src/app.js
+++ src/app.js
@@ -2,46 +2,40 @@
 App = {
     loading: false,
     contracts: {},
-  
+
     load: async () => {
-      await App.loadWeb3()
+      await App.loadWeb3(); 
       await App.loadAccount()
-      await App.loadContract()
-      await App.render()
+      await App.loadContract() 
+      await App.render() 
     },
-    loadWeb3: async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum) // give provider as w.e to web3
-        App.web3Provider=window.ethereum  // save provider in app
-        try {
-          // Request account access if needed
-          await ethereum.enable()
-          // Acccounts now exposed
-          web3.eth.sendTransaction({/* ... */})
-        } catch (error) {
-          // User denied account access...
-        }
-      } // Non-dapp browsers...
-      else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
-      }
+    loadWeb3:async()=>{ 
+      const web3 = new Web3(Web3.givenProvider); // is metamask provider 
+      window.web3 =web3
+      web3.eth.net.isListening()
+         .then(()=>{
+           console.log("Web3 is connected")
+         })
+         .catch(e => console.log('Wow. Something went wrong'));
+       
     },
-  
     loadAccount: async () => {
       // Set the current blockchain account
       const accounts = await ethereum.request({ method: 'eth_accounts' }); 
       App.account = accounts[0] 
     },
   
-    loadContract: async () => {
-        const contactList=await $.getJSON('Contacts.json')
-        App.contracts.ContactList=TruffleContract(contactList)
-        App.contracts.ContactList.setProvider(App.web3Provider)
-        // Hydrate the smart contract with values from the blockchain
-        App.contactList=await App.contracts.ContactList.deployed()
+    loadContract:async()=>{
+         // connect to contract
+      const jsonContract=await $.getJSON('Contacts.json')
+      const contract_Address="0x702f1C30b26Af124209025161c58068216b2602C"; 
+      const contract = new web3.eth.Contract(jsonContract.abi, contract_Address, {
+        from: '0x64091cF01f32C104DbBB80b57C537738d171A253', // default from address
+        gasPrice: '1000000' // default gas price in wei, 20 gwei in this case
+      }); 
+      App.contactList=contract  
+      contract.methods.totalContacts().call().then(console.log);
     },
-  
     render: async () => {
       // Prevent double render
       if (App.loading) {
@@ -54,20 +48,19 @@ App = {
       // Render Account
       $('#account').html(App.account) 
       // Render Tasks
-      await App.renderTasks()
+      await App.renderContacts()
   
       // Update loading state
       App.setLoading(false)
     },
-  
-    renderTasks: async () => {
+    renderContacts: async () => {
       // Load the total task count from the blockchain
-      const contactCounts = await App.contactList.totalContacts()
+      const contactCounts = await App.contactList.methods.totalContacts().call()
     
       // Render out each task with a new task template
       for (var i = 1; i <= contactCounts; i++) {
         // Fetch the contact data from the blockchain
-        const contact = await App.contactList.contacts(i)
+        const contact = await App.contactList.methods.contacts(i).call()
         const id=contact[0]
         const name = contact[1]
         const phone = contact[2]
@@ -97,7 +90,12 @@ App = {
       const name = $('#contactName').val()
       const phone = $('#contactPhone').val()
       const desc = $('#desc').val()
-      await App.contactList.createContact(name,phone,desc,{from:App.account})
+      const gas = await  App.contactList.methods.createContact(name,phone,desc).estimateGas();
+      const saveContact = await  App.contactList.methods.createContact(name,phone,desc).send({
+        from: App.account,
+        gas,
+      }); 
+      console.log(saveContact)
       window.location.reload()
     },
     popupEdit:(id,name,phone,desc)=>{ 
@@ -112,14 +110,24 @@ App = {
         const phone = $('#edPhone').val()
         const desc = $('#edDesc').val()
         const id = $('#cid').val()
-       await App.contactList.editContact(id,name,phone,desc,{from:App.account})
+        const gas = await  App.contactList.methods.editContact(id,name,phone,desc).estimateGas();
+        const editContact = await  App.contactList.methods.editContact(id,name,phone,desc).send({
+          from: App.account,
+          gas,
+        });  
+        console.log(editContact)
        $('#editContactModel').modal('hide');
        App.setLoading(true)
        window.location.reload()
     },
     deleteContact:async(id)=>{
         App.setLoading(true)
-        await App.contactList.deleteContact(id,{from:App.account})
+        const gas = await  App.contactList.methods.deleteContact(id).estimateGas();
+        const deleteContact = await  App.contactList.methods.deleteContact(id).send({
+          from: App.account,
+          gas,
+        });  
+        console.log(deleteContact) 
         window.location.reload()
     },
     setLoading: (boolean) => {
--- truffle-config.js
+++ truffle-config.js
@@ -17,8 +17,8 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -57,14 +57,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    gorili: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 5,       // Ropsten's id
+      gas: 10000000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -83,13 +83,13 @@ module.exports = {
     solc: {
       version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
     }
   },
 
